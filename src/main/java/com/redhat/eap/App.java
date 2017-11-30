b5d325393415d6dc748d4c69075bae8303a53740
Torben Jaeger <torben@jit-central.com>
2017-11-30 23:19:22 +0100

    bugfix

--- src/main/java/com/redhat/eap/App.java
+++ src/main/java/com/redhat/eap/App.java
@@ -1,8 +1,6 @@
 package com.redhat.eap;
 
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -16,7 +14,6 @@ public class App {
     private static Map<MyLeakingObject, String> leakingMap = new HashMap<>();
 
     @Path("/leakIt")
-    @Produces(MediaType.APPLICATION_JSON)
     public String leakIt() {
         for (int i = 0; i < 10000; i++) {
             leakingMap.put(new MyLeakingObject("aname"), UUID.randomUUID().toString());
